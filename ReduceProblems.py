6c67728fb7c6d1ccc9ec3e2cf728761e30a40e7b
Bernabe Jimenez <bernabejimenez@MacBook-Pro-de-Bernabe.local>
2020-04-06 14:06:29 +0200

    reduces primer elemento distinto e indices de lista

---
+++ ReduceProblems.py
@@ -0,0 +1,21 @@
+from functools import reduce
+
+def ProductoTotal(l):
+    resultado = 1
+    for valor in l:
+        resultado *= valor
+    return resultado
+
+lista = [1, 3, -1, 15, 9]
+
+sumatorio = reduce(lambda x, y: x + y, lista)
+
+#creo una copia de lista
+l = lista[:]
+
+#añado el neutro para la suma en la posición cero
+l.insert(0,0)
+sumatorioDobles = reduce(lambda x, y: x + y*2, l)
+
+print(sumatorio)
+print(sumatorioDobles)
